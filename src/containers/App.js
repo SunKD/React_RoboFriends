ca81750776392fad3550472520f51993208e200e
Sun Davey <sundavey@hotmail.com>
2018-07-14 22:59:20 -0700

    cleaned the code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../component/CardList';
+import SearchBox from '../component/SearchBox';
+import Scroll from '../component/Scroll';
 
 class App extends Component {
     constructor() {
@@ -23,10 +23,11 @@ class App extends Component {
         this.setState({ searchfield: event.target.value })
     }
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if(this.state.robots.length === 0){
+        if(robots.length === 0){
             return <h1>Loading</h1>
         }else{
             return (
